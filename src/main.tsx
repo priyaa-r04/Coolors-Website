ef8e279752af0292b2181a7146172479092c9c0b
priya <priyareddy@thoughtwin.com>
2025-05-15 16:17:04 +0530

    fixed route issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import { store } from './redux/store.tsx';
@@ -8,9 +7,7 @@ import { store } from './redux/store.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter> {/* ✅ Wrap your App here */}
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>
 );
