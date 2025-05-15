f4549f9b8c272c5312612d064249f1fdfdb4783d
priya <priyareddy@thoughtwin.com>
2025-05-15 16:05:01 +0530

    changes done in main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import { store } from './redux/store.tsx';
@@ -7,7 +8,9 @@ import { store } from './redux/store.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter> {/* ✅ Wrap your App here */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
